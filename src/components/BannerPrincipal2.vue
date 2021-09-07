20bd44247e8096f7c3d788a2ab0a43adea8c85df
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-09-07 12:51:56 -0500

    Pruebas banner

--- src/components/BannerPrincipal2.vue
+++ src/components/BannerPrincipal2.vue
@@ -16,10 +16,11 @@
 
       .d-none.d-lg-block.col-lg-5.px-0.banner-principal__img
         .contenedor-imagenes
-          .imagen_flotante.imagen_flotante_1: img(src="@/assets/curso/float1.svg")
-          .imagen_flotante.imagen_flotante_2: img(src="@/assets/curso/float1.svg")
-          .imagen_flotante.imagen_flotante_3: img(src="@/assets/curso/float1.svg")
-          .imagen_flotante.imagen_flotante_4: img(src="@/assets/curso/float1.svg")
+          - const droplets = 20
+            .rain
+              - for (let r = 0; r < droplets; r++)
+                svg.rain__drop(preserveAspectRatio="xMinYMin", viewBox='0 0 5 50', style=`--x: ${Math.floor(Math.random() * 100)}; --y: ${Math.floor(Math.random() * 100)}; --o: ${Math.random()}; --a: ${Math.random() + 0.5}; --d: ${(Math.random() * 2) - 1}; --s: ${Math.random()}`)
+                  path(stroke='none', d="M 2.5,0 C 2.6949458,3.5392017 3.344765,20.524571 4.4494577,30.9559 5.7551357,42.666753 4.5915685,50 2.5,50 0.40843152,50 -0.75513565,42.666753 0.55054234,30.9559 1.655235,20.524571 2.3050542,3.5392017 2.5,0 Z")
           img(:src="globalData.imagenBannerPrincipal")
 </template>
 
@@ -144,4 +145,33 @@ export default {
     opacity: 1
 	100%
     opacity: 0
+
+body
+  background-color: #6c78a9
+  display: flex
+  align-items: center
+  justify-content: center
+
+.rain__drop
+  animation-delay: calc(var(--d) * 3s)
+  animation-duration: calc(var(--a) * 3s)
+  animation-iteration-count: infinite
+  animation-name: drop
+  animation-timing-function: linear
+  height: 60px
+  left: calc(var(--x) * 1%)
+  position: absolute
+  top: calc((var(--y) + 0) * -1px)
+
+  path
+    fill: #a1c6cc
+    opacity: var(--o)
+    transform: scaleY(calc(var(--s) * 1.5))
+
+@keyframes drop
+  90%
+    opacity: 1
+  100%
+    opacity: 0
+    transform: translateY(40vh)
 </style>
