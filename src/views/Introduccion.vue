b9bf0d14b186fcdca80f5f98a4cac31ac5d800b2
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-10-29 14:54:14 -0500

    Video introducción cargado

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -15,8 +15,7 @@
       .col-lg-5: img(src='@/assets/curso/img0.svg')
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/nEMlXW26m2U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 </template>
 
